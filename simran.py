e8a33be553bb5b43e9f540cc8fccc584658a7ae7
Simran <simransingh20226@gmail.com>
2026-04-22 18:24:09 +0530

    Day 2: practiced basic Python

--- simran.py
+++ simran.py
@@ -1,67 +1,163 @@
-name = input("Enter your name: ")
-print("Hello", name + ", welcome to Python!")
+# name = input("Enter your name: ")
+# print("Hello", name + ", welcome to Python!")
 
 
 
-# 2. Calculator
-a = int(input("Enter first number: "))
-b = int(input("Enter second number: "))
+# # 2. Calculator
+# a = int(input("Enter first number: "))
+# b = int(input("Enter second number: "))
 
-print("Sum:", a + b)
-print("Difference:", a - b)
-print("Product:", a * b)
-print("Division:", a / b)
+# print("Sum:", a + b)
+# print("Difference:", a - b)
+# print("Product:", a * b)
+# print("Division:", a / b)
 
 
 
-# 3. Even or Odd
-num = int(input("Enter a number: "))
-if num % 2 == 0:
-    print("Even")
+# # 3. Even or Odd
+# num = int(input("Enter a number: "))
+# if num % 2 == 0:
+#     print("Even")
+# else:
+#     print("Odd")
+
+
+
+# # 4. Largest of Two Numbers
+# x = int(input("Enter first number: "))
+# y = int(input("Enter second number: "))
+
+# if x > y:
+#     print(x, "is greater")
+# else:
+#     print(y, "is greater")
+
+
+
+# # 5. Swap Two Numbers (without third variable)
+# p = int(input("Enter first number: "))
+# q = int(input("Enter second number: "))
+
+# p, q = q, p
+
+# print("After swapping:")
+# print("p =", p)
+# print("q =", q)
+
+
+# # 6. Square and Cube
+# n = int(input("Enter a number: "))
+# print("Square:", n ** 2)
+# print("Cube:", n ** 3)
+
+
+# # 7. Simple Interest
+# P = float(input("Enter principal: "))
+# R = float(input("Enter rate: "))
+# T = float(input("Enter time: "))
+
+# SI = (P * R * T) / 100
+# print("Simple Interest:", SI)
+
+
+
+# # 8. Temperature Converter
+# celsius = float(input("Enter temperature in Celsius: "))
+# fahrenheit = (celsius * 9/5) + 32
+# print("Temperature in Fahrenheit:",
+#  fahrenheit)
+
+
+
+# conditional statements
+# age=23
+
+# if (age >=18):
+#     print("can vote and apply for licensce")
+# #     print("can drive too")
+
+# light="red"
+
+# if (light =="pink"):
+#     print("stop")
+# elif( light =="green"):
+#     print("go")
+# elif( light =="yellow") :
+#     print("look")
+# else:
+#     print("light is broken")
+
+# age=14
+
+# if (age >=18):
+#     print("can vote and be finanviallyu independent") 
+# else:
+#     print("you are still a kid according to society..but who cares ")
+
+
+# grade students according to their marks 
+
+# marks=88
+# marks=int(input("enter student marks:"))
+
+# if(marks >=90):
+#     grade="A"
+# elif (marks >= 80 and marks < 90) :
+#     grade="B"
+# elif (marks >= 70 and marks < 80) :
+#     grade="C"
+# elif (marks >= 60 and marks < 70) :
+#     grade="D"
+# else:
+#     print("you are a losser according to brainwashed people")
+
+# nesting is writting one statement into another statement 
+
+# age=98
+
+# if (age >=18) :
+#     if (age >=80):
+#         print("cannot drive ")
+#     else:
+#         print("can drive ")
+# else:
+#     print("cannot drive ")
+
+#  check if a number is odd or even 
+
+# num= int(input("enter a number :"))
+
+# if num % 2 ==0:
+#     print("even number")
+# else:
+#     print("odd number ")
+
+# find greatest of 3 numbers 
+
+# a=int(input("enter first number :"))
+# b=int(input("enter second  number :"))
+# c=int(input("enter third number :"))
+
+# if a >= b and a >= c :
+#     print("greatest is :" , a)
+# elif b >= a and b >=c :
+#     print("greatest is :" , b)
+# else:
+#     print("greatest is :" , c)
+
+#  check if a number is multiple of 7 
+
+num=int(input("Enter a number : "))
+
+if num % 7 ==0:
+    print("it is a multiple of 7")
 else:
-    print("Odd")
+    print("not multiple of 7")
 
 
 
-# 4. Largest of Two Numbers
-x = int(input("Enter first number: "))
-y = int(input("Enter second number: "))
-
-if x > y:
-    print(x, "is greater")
-else:
-    print(y, "is greater")
 
 
 
-# 5. Swap Two Numbers (without third variable)
-p = int(input("Enter first number: "))
-q = int(input("Enter second number: "))
-
-p, q = q, p
-
-print("After swapping:")
-print("p =", p)
-print("q =", q)
 
 
-# 6. Square and Cube
-n = int(input("Enter a number: "))
-print("Square:", n ** 2)
-print("Cube:", n ** 3)
-
-
-# 7. Simple Interest
-P = float(input("Enter principal: "))
-R = float(input("Enter rate: "))
-T = float(input("Enter time: "))
-
-SI = (P * R * T) / 100
-print("Simple Interest:", SI)
-
-
-
-# 8. Temperature Converter
-celsius = float(input("Enter temperature in Celsius: "))
-fahrenheit = (celsius * 9/5) + 32
-print("Temperature in Fahrenheit:", fahrenheit)
